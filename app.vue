6966c763795de9f63161f00965dee2745d39db40
Demola <64336052+ekogeda@users.noreply.github.com>
2023-06-13 23:16:38 +0100

    Installation of vuetify

--- app.vue
+++ app.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
-    <NuxtWelcome />
-  </div>
-</template>
+    <v-card class="mx-auto" width="400" prepend-icon="mdi-home">
+      <template v-slot:title>
+        Hello from Vuetify!
+      </template>
+      <v-card-text>
+        When you see this inside a card, it worked!
+      </v-card-text>
+  </v-card>
+  <NuxtWelcome />
+</div></template>
