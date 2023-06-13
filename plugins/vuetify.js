c83fddae8fe6c5af63f9880da40e5c46e73a2066
Demola <64336052+ekogeda@users.noreply.github.com>
2023-06-13 23:56:04 +0100

    Setting default theme to dark

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -7,6 +7,35 @@ export default defineNuxtPlugin(nuxtApp => {
     ssr: true,
     components,
     directives,
+    theme: {
+      defaultTheme: "dark",
+      // themes: {
+      //   light: {
+      //     dark: false,
+      //     colors: {
+      //       primary: "#ad1c3d",
+      //       "page-header-background": "#d7d7ce",
+      //       "page-background": "#cdcdc1",
+      //       "table-header": "#cdcdc1",
+      //       background: "#c0c0b5",
+      //       "header-background": "#b5b5a6",
+      //       "info-text": "#666660",
+      //     },
+      //   },
+      //   dark: {
+      //     dark: true,
+      //     colors: {
+      //       primary: "#52E3C2",
+      //       "page-header-background": "#282831",
+      //       "page-background": "#32323E",
+      //       "table-header": "#2e2e2e",
+      //       background: "red",
+      //       "header-background": "#4a4a59",
+      //       "info-text": "#99999F",
+      //     },
+      //   },
+      // },
+    }
   })
 
   nuxtApp.vueApp.use(vuetify)
