37affc38acc6c79b422d07bbe34eae15aa417c46
Demola <64336052+ekogeda@users.noreply.github.com>
2023-06-13 23:40:06 +0100

    Front page update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,7 @@
 export default defineNuxtConfig({
 	devtools: { enabled: true },
-	alias: {
-		assets: '/<rootDir>/assets',
-	},
-	css: ['vuetify/lib/styles/main.sass', '@mdi/font/css/materialdesignicons.min.css'],
+	alias: { assets: '/<rootDir>/assets' },
+	css: ['~/assets/main.scss', 'vuetify/lib/styles/main.sass', '@mdi/font/css/materialdesignicons.min.css'],
 	build: {
 		transpile: ['vuetify'],
 	},
