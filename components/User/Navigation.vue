534235002c10126c07f9cee3a32f86a3cdcc3946
Demola <64336052+ekogeda@users.noreply.github.com>
2023-06-14 01:40:17 +0100

    Added navigation for user

---
+++ components/User/Navigation.vue
@@ -0,0 +1,45 @@
+<script setup>
+const drawer = ref(true)
+const rail = ref(true)
+
+const items = [
+  { title: 'Dashboard', icon: 'mdi-home-city' },
+  { title: 'Profile', icon: 'mdi-account-circle' },
+  { title: 'Order', icon: 'mdi-van-utility' },
+  { title: 'Track', icon: 'mdi-access-point-network' },
+  { title: 'Get Estimate', icon: 'mdi-calculator' },
+  { title: 'Report', icon: 'mdi-chart-line' },
+  { title: 'Transaction', icon: 'mdi-alpha-t-circle-outline' },
+  { title: 'Shopping', icon: 'mdi-shopping-outline' },
+  { title: 'Wallet', icon: 'mdi-wallet-bifold-outline' },
+  { title: 'Settings', icon: 'mdi-cog-outline' },
+  { title: 'Logout', icon: 'mdi-power' },
+]
+</script>
+
+<template>
+  <v-navigation-drawer v-model="drawer" :rail="rail" permanent @click="rail = false">
+    <v-list-item-title class="my-3 text-center">
+      <nuxt-link to="/">
+        <img v-show="$vuetify.theme.name == 'dark'" src="@/assets/aaj/aaj-logo.png" width="50" alt="logo" />
+        <img v-show="$vuetify.theme.name == 'light'" src="@/assets/aaj/logo.png" width="50" alt="logo" />
+      </nuxt-link>
+    </v-list-item-title>
+
+    <v-divider></v-divider>
+
+    <v-list-item title="James Smith" prepend-avatar="https://randomuser.me/api/portraits/men/85.jpg" nav>
+      <template v-slot:append>
+        <v-btn variant="text" icon="mdi-chevron-left" @click.stop="rail = !rail"></v-btn>
+      </template>
+    </v-list-item>
+
+    <v-list density="compact" nav>
+      <v-list-item v-for="(item, i) in  items " :key="i" :prepend-icon="item.icon" :title="item.title" :value="item.title"
+        @click="$emit('handleRouteChange', (item.title).toLowerCase())"></v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+
+<style lang="scss" scoped></style>
--- layouts/users.vue
+++ layouts/users.vue
@@ -12,30 +12,13 @@ const theme = useTheme();
 const toggleTheme = () =>
   (theme.global.name.value = theme.global.current.value.dark ? "light" : "dark");
 
-const drawer = ref(true)
-const rail = ref(true)
-
-const items = [
-  { title: 'Dashboard', icon: 'mdi-home-city' },
-  { title: 'Profile', icon: 'mdi-account-circle' },
-  { title: 'Order', icon: 'mdi-van-utility' },
-  { title: 'Track', icon: 'mdi-access-point-network' },
-  { title: 'Get Estimate', icon: 'mdi-calculator' },
-  { title: 'Report', icon: 'mdi-chart-line' },
-  { title: 'Transaction', icon: 'mdi-alpha-t-circle-outline' },
-  { title: 'Shopping', icon: 'mdi-shopping-outline' },
-  { title: 'Wallet', icon: 'mdi-wallet-bifold-outline' },
-  { title: 'Settings', icon: 'mdi-cog-outline' },
-  { title: 'Logout', icon: 'mdi-power' },
-]
-
 const icons = [
   { title: 'Search', icon: 'mdi-magnify' },
   { title: 'Notification', icon: 'mdi-bell-badge' },
   { title: 'Settings', icon: 'mdi-cog' }
 ]
 
-const onChangeLink = (params) => {
+const onChangeRoute = (params) => {
   if (params != 'logout') {
     console.log('Link:', params)
     // $toast.success('You did it!', { position: 'top-right' });
@@ -53,27 +36,7 @@ const onChangeLink = (params) => {
   <div class="admin-layout">
     <v-card>
       <v-layout>
-        <v-navigation-drawer v-model="drawer" :rail="rail" permanent @click="rail = false">
-          <v-list-item-title class="my-3 text-center">
-            <nuxt-link to="/">
-              <img v-show="$vuetify.theme.name == 'dark'" src="@/assets/aaj/aaj-logo.png" width="50" alt="logo" />
-              <img v-show="$vuetify.theme.name == 'light'" src="@/assets/aaj/logo.png" width="50" alt="logo" />
-            </nuxt-link>
-          </v-list-item-title>
-
-          <v-divider></v-divider>
-
-          <v-list-item title="James Smith" prepend-avatar="https://randomuser.me/api/portraits/men/85.jpg" nav>
-            <template v-slot:append>
-              <v-btn variant="text" icon="mdi-chevron-left" @click.stop="rail = !rail"></v-btn>
-            </template>
-          </v-list-item>
-
-          <v-list density="compact" nav>
-            <v-list-item v-for="(item, i) in items" :key="i" :prepend-icon="item.icon" :title="item.title"
-              :value="item.title" @click="onChangeLink((item.title).toLowerCase())"></v-list-item>
-          </v-list>
-        </v-navigation-drawer>
+        <UserNavigation @handleRouteChange="onChangeRoute" />
 
         <v-app-bar fixed :color="$vuetify.theme.name == 'dark' ? 'grey-darken-4' : 'white'">
           <v-toolbar title="Dashboard" :color="$vuetify.theme.name == 'dark' ? 'grey-darken-4' : 'white'" dark>
